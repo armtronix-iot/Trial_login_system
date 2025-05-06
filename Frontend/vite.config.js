6b8bd4eef14292bd0d9fce1fcb5e92be45bd8c32
armtronix-iot <armtronixiot@gmail.com>
2025-05-06 16:32:12 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,3 +1,26 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5176,
+//     proxy: {
+//       '/api': {
+//         target: 'https://trial-login-system-backend.onrender.com',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+//   build: {
+//     outDir: 'dist',
+//   }
+// });
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,15 +29,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5176,
-    proxy: {
-      '/api': {
-        target: 'https://trial-login-system-backend.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
+    allowedHosts:['team.armtronix.net']
   }
 });
